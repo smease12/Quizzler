c810b067538160946cd814256f22b5d2fa7c9e1f
smease12 <scmease93@gmail.com>
2024-01-13 18:10:12 -0500

    Update question label on viewDidLoad

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -11,13 +11,12 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        questionLabel.text = "Four + Two is equal to Six"
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
