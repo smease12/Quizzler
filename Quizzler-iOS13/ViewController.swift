568fccee47d20e8c9042d2f1a8b821ca05c25735
smease12 <scmease93@gmail.com>
2024-01-13 18:21:20 -0500

    Create quiz array to hold question strings

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -14,9 +14,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    let quiz = [
+        "Four + Two is equal to Six",
+        "Five - Three is greater than One",
+        "Three + Eight is less than Ten"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLabel.text = "Four + Two is equal to Six"
+        questionLabel.text = quiz[0]
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
