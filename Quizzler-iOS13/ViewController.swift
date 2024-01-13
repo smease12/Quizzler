63335bbe010c77a858263813571647702bc8e3ba
smease12 <scmease93@gmail.com>
2024-01-13 18:03:08 -0500

    Create buttonPressed action connected to the two buttons

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
 }
 
