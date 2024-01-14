8b9d75733b2c4a9c4c38a54e53af431700f8fb3f
smease12 <scmease93@gmail.com>
2024-01-13 20:48:38 -0500

    Make button green or red for 0.2 seconds on button pressed based on if it was correct or not

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -42,10 +42,10 @@ class ViewController: UIViewController {
         let actualAnswer = quiz[questionNumber].answer
         
         if userAnswer == actualAnswer{
-            print("Right!")
+            sender.backgroundColor = UIColor.green
         }
         else{
-            print("Wrong!")
+            sender.backgroundColor = UIColor.red
         }
         
         if questionNumber + 1 < quiz.count{
@@ -55,12 +55,15 @@ class ViewController: UIViewController {
             questionNumber = 0
         }
         
-        updateUI()
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        
+        
     }
     
-    func updateUI(){
+    @objc func updateUI(){
         questionLabel.text = quiz[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
     }
-    
 }
 
